3973074e1e8ed7a2ad23f573fb920614d30f1cf5
Anatoly <u123@ua.fm>
2024-06-11 14:18:44 +0300

    borrowings view is only for auth users

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
@@ -8,6 +9,7 @@ from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerialize
 class BorrowingsViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     # serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated, )
 
     def get_serializer_class(self):
         if self.action in ("create", "update", ):
