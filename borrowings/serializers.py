3fc01f07d8d8dc15a0da0e8d94f1c753bac58bfd
Anatoly <u123@ua.fm>
2024-06-11 12:32:14 +0300

    create BorrowingCreate serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,13 +1,32 @@
+from django.db import transaction
 from rest_framework import serializers
 
+from books.models import Book
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(slug_field="email", read_only=True)
-    book = BookSerializer(read_only=True)
+    # book = BookSerializer(read_only=True)
+    book = serializers.StringRelatedField()
+
     class Meta:
         model = Borrowing
-        fields = ("id", "book", "user", "borrow_date", "expected_return_date", "actual_return_date", "is_active", )
+        fields = (
+            "id",
+            "book",
+            "user",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "is_active",
+        )
         read_only_fields = ("is_active", )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "user", "borrow_date", "expected_return_date", "actual_return_date", )
