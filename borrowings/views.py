90adf6af6c8c30569c4606b958905d5e059a77ab
Anatoly <u123@ua.fm>
2024-06-12 20:08:22 +0300

    add return_borrowing action in view

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer, BorrowingReturnSerializer
 
 
 class BorrowingsViewSet(viewsets.ModelViewSet):
@@ -14,6 +16,8 @@ class BorrowingsViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action in ("create", "update", ):
             return BorrowingCreateSerializer
+        if self.action == "return_borrowing":
+            return BorrowingReturnSerializer
         return BorrowingSerializer
 
     def get_queryset(self):
@@ -31,3 +35,17 @@ class BorrowingsViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(actual_return_date__isnull=True)
 
         return queryset
+
+    @action(
+        methods=["POST", ],
+        detail=True,
+        url_path="return",
+        # permission_classes=[IsAuthenticated, ],
+    )
+    def return_borrowing(self, request, pk=None):
+        """Endpoint for returning borrowing"""
+        borrowing = self.get_object()
+        serializer = self.get_serializer(borrowing, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
