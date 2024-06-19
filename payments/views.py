de61bad4effc8bf26f13d79459bb068795d1bf64
Anatoly <u123@ua.fm>
2024-06-19 14:30:23 +0300

    access for only own payments & all for admin

--- payments/views.py
+++ payments/views.py
@@ -9,4 +9,12 @@ from payments.serializers import PaymentSerializer
 class PaymentsViewSet(ReadOnlyModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = super().get_queryset()
+        if not user.is_superuser:
+            queryset = queryset.filter(borrowing__user_id=user.id)
+        return queryset
+
