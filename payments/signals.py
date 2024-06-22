20b1fd92cb08151f55e7f795164f02162e94f3cf
Anatoly <u123@ua.fm>
2024-06-23 02:22:23 +0300

    update signal post_save function

--- payments/signals.py
+++ payments/signals.py
@@ -8,11 +8,18 @@ from payments.models import Payment
 @receiver(post_save, sender=Payment)
 def send_msg_after_create(sender, instance, created, **kwargs):
     if created:
-        message = (f"*Payment Checkout has been created.* \n"
+        if instance.type == Payment.Type.PAYMENT:
+            date_from = instance.borrowing.borrow_date
+            date_to = instance.borrowing.expected_return_date
+        else:
+            date_from = instance.borrowing.expected_return_date
+            date_to = instance.borrowing.actual_return_date
+
+        message = (f"*{instance.type.capitalize()} Checkout has been created.* \n"
                    f"Amount: {instance.money_to_pay}\n"
                    f"Borrowing id: {instance.borrowing.id} | Book: {instance.borrowing.book.title} | "
                    f"User: {instance.borrowing.user}\n"
-                   f"From: {instance.borrowing.borrow_date} To: {instance.borrowing.expected_return_date}\n"
+                   f"From: {date_from} To: {date_to}\n"
                    f"Status: {instance.type} : {instance.status}")
         bot.send_message(message)
 
