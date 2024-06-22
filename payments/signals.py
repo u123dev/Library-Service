068b1a881a4e7266768d8b6058bbc6440f0314bf
Anatoly <u123@ua.fm>
2024-06-23 02:32:02 +0300

    refactor signal post_save function

--- payments/signals.py
+++ payments/signals.py
@@ -3,24 +3,13 @@ from django.dispatch import receiver
 
 from notifications.services import bot
 from payments.models import Payment
+from payments.services import detail_payment_info
 
 
 @receiver(post_save, sender=Payment)
-def send_msg_after_create(sender, instance, created, **kwargs):
+def send_msg_after_save(sender, instance, created, **kwargs):
     if created:
-        if instance.type == Payment.Type.PAYMENT:
-            date_from = instance.borrowing.borrow_date
-            date_to = instance.borrowing.expected_return_date
-        else:
-            date_from = instance.borrowing.expected_return_date
-            date_to = instance.borrowing.actual_return_date
-
-        message = (f"*{instance.type.capitalize()} Checkout has been created.* \n"
-                   f"Amount: {instance.money_to_pay}\n"
-                   f"Borrowing id: {instance.borrowing.id} | Book: {instance.borrowing.book.title} | "
-                   f"User: {instance.borrowing.user}\n"
-                   f"From: {date_from} To: {date_to}\n"
-                   f"Status: {instance.type} : {instance.status}")
+        message = detail_payment_info(instance)
         bot.send_message(message)
 
     if kwargs.get("update_fields") and "status" in kwargs.get("update_fields"):
