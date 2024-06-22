0b9511f2b9b61fc41b43709ee923da8dc3d33f76
Anatoly <u123@ua.fm>
2024-06-22 16:36:41 +0300

    create Success endpoint

--- payments/views.py
+++ payments/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from payments.models import Payment
-from payments.serializers import PaymentSerializer
+from payments.serializers import PaymentSerializer, PaymentSuccessSerializer
+from payments.services import set_payment_status_paid
 
 
 class PaymentsViewSet(ReadOnlyModelViewSet):
@@ -17,4 +21,26 @@ class PaymentsViewSet(ReadOnlyModelViewSet):
         if not user.is_superuser:
             queryset = queryset.filter(borrowing__user_id=user.id)
         return queryset
+
+    @action(
+        methods=["GET", ],
+        detail=False,
+        url_path="success",
+    )
+    def success(self, request):
+        """Endpoint for success payment"""
+        serializer = PaymentSuccessSerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        session_id = serializer.validated_data.get("session_id")
+
+        if (e := set_payment_status_paid(session_id)) is True:
+
+            return Response("Payment Successful", status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(
+            {"error": str(e)},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 
