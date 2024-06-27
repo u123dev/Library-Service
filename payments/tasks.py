725737a61a5a6f41813a0636ba8281b778bd4169
Anatoly <u123@ua.fm>
2024-06-27 11:03:27 +0300

    update check_expired_session() task

--- payments/tasks.py
+++ payments/tasks.py
@@ -1,16 +1,11 @@
-import stripe
-
 from payments.models import Payment
+from payments.services import set_payment_status_expired
 
 
 def check_expired_session():
     '''Check Stripe Sessions for expiration'''
 
     payments = Payment.objects.filter(status=Payment.StatusType.PENDING)
-    res = dict()
-    for payment in payments:
-        session_id = payment.session_id
-        session = stripe.checkout.Session.retrieve(session_id)
-        res[session_id] = session.status
-        print(session_id, session.status)
+    res = {payment.session_id : payment.status
+           for payment in payments if (status := set_payment_status_expired(payment))}
     return res
