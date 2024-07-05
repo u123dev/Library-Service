a0cd683ec380a2b177990f9e509dc489d0176c52
Anatoly <u123@ua.fm>
2024-07-05 09:40:46 +0300

    Payment request profiling

--- payments/tasks.py
+++ payments/tasks.py
@@ -5,7 +5,7 @@ from payments.services import set_payment_status_expired
 def check_expired_session():
     '''Check Stripe Sessions for expiration'''
 
-    payments = Payment.objects.filter(status=Payment.StatusType.PENDING)
+    payments = Payment.objects.filter(status=Payment.StatusType.PENDING).select_related()
     res = {payment.session_id : payment.status
            for payment in payments if set_payment_status_expired(payment)}
     return res
