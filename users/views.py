7121cbb742825b8d06bcaedfa80c3d0e6c469930
Anatoly <u123@ua.fm>
2024-07-16 14:13:03 +0300

    documentation for user

--- users/views.py
+++ users/views.py
@@ -6,14 +6,17 @@ from users.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
+    """Create new user"""
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage user profile"""
     serializer_class = UserSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
+        """Get user"""
         return self.request.user
